0f3f5040446bddba993e627437073473d8d4f8b0
@LaibaAshfaq2 <laibaashfaqahmed232@gmail.com>
2025-02-07 18:28:41 +0500

    order placed

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion } from '../env'
+import { apiVersion, dataset, projectId,token } from '../env'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
-  token: process.env.SANITY_API_TOKEN,
+  projectId,
+  dataset,
+  token,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
